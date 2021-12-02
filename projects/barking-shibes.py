029b39da48b944bbcfbf05726f844297344c498e
mgta2 <matthew.g.t.adams@gmail.com>
2021-12-02 17:37:50 +0000

    final two projects commit

--- projects/barking-shibes.py
+++ projects/barking-shibes.py
@@ -7,3 +7,30 @@
 # Write the code logic to make the API calls and assign the output to
 # `doged_quote` and `doge_image_url` respectively.
 # Then write the `html` string to a `.html` file and look at it in your browser.
+
+# -----------------------------------------------------------------------------
+
+# Following along with https://www.youtube.com/watch?v=4WNMMOk958A&t=423s&ab_channel=CodingNomads
+# for the HTML at the end.
+
+import requests
+from pathlib import Path
+
+quotes_url = "http://quotes.stormconsultancy.co.uk/random.json"
+images_url = "http://shibe.online/api/shibes"
+
+quote_response = requests.get(quotes_url)
+image_response = requests.get(images_url)
+
+quote = quote_response.json()['quote']
+doged_quote = "Wow! " + quote + " Much quote."
+
+doge_image_url = image_response.json()[0]
+
+# With the necessary info gathered, it's time to make the html file.
+
+html = f"<p>{doged_quote}</p><img src='{doge_image_url}'>"
+file_path = Path().home().joinpath("Desktop").joinpath("dogescript.html")
+
+with open(file_path, "w") as file_out:
+    file_out.write(html)
--- projects/weather-game.py
+++ projects/weather-game.py
@@ -1,3 +1,105 @@
 # Include the current weather into your game mechanics.
 
-URL = "https://www.metaweather.com/api/"
+URL = "https://www.metaweather.com/api/location/44418/"
+
+# Project: Adventure Game updated to include weather API above.
+
+import requests
+import random
+
+print("Welcome to the adventure game.")
+
+response = requests.get(URL)
+weather_state = response.json()["consolidated_weather"][0]["weather_state_name"]
+
+print("You wake up in a strange room, in London, UK.\n"
+      "Because I have to implement a weather API, there is a small window.\n"
+      f"From this window you can see that outside the weather is {weather_state.lower()}."
+      )
+print()
+print("There are 3 doors before you...")
+print("Input 1, 2 or 3 to enter the respective door.")
+inventory = set()
+
+# Randomly assign the doors to objects behind them.
+
+dragon = random.randint(0,2)
+binary = random.randint(0,1)
+if binary == 0:
+    zombie = (1 + dragon) % 3
+    empty = (dragon - 1) % 3
+else:
+    zombie = (dragon - 1) % 3
+    empty = (1 + dragon) % 3
+
+zombie += 1
+dragon += 1
+empty += 1
+
+while True:
+    user_input = input("Which door do you choose?: ")
+    if user_input.isdigit():
+        user_input = int(user_input)
+        if not(user_input in [1, 2, 3]):
+            print("You did not enter 1, 2 or 3.")
+            continue
+    else:
+        print("You did not enter an integer.")
+        continue
+    
+    if user_input == empty:
+        print("You enter a seemingly empty room.")
+        user_input = input("Type 'explore' to look around, else leave: ")
+        if user_input == "explore":
+            print("You find a sword! It is added to your inventory and you leave the room.")
+            inventory.add("sword")
+            continue
+    
+    if user_input == zombie:
+        print("You enter a room with a zombie!")
+        user_input = input("Type 'attack' or leave the room: ")
+        if user_input == "attack":
+            if "sword" in inventory:
+                print("Your sword cuts down the zombie!")
+                print("You see the zombie was guarding some armour.")
+                print("The armour is added to your inventory and you leave the room.")
+                inventory.add("armour")
+                continue
+            else:
+                print("Weaponless, the zombie kills you swiftly.")
+                print("--- Game Over ---")
+                break
+    
+    if user_input == dragon:
+        print("You enter the room to see a dragon!")
+        user_input = input("Type 'attack' or leave the room: ")
+        if user_input == "attack":
+            
+            # Sword gives 50% chance of victory. Armour ensures survival.
+            if "sword" in inventory:
+                success = random.randint(0,1)
+                if success == 0:
+                    print("The dragon sees your attack coming and crushes your sword.")
+                    inventory.remove("sword")
+                    
+                    if "armour" in inventory:
+                        print("Your armour saves you from death, but your sword is gone.")
+                        print("You retreat back from where you came, to search for another weapon.")
+                        continue
+                    else:
+                        print("With no armour, you die.")
+                        print("--- Game Over ---")
+                        break
+                    
+                else:
+                    print("Your blade strikes the dragon's weak spot!")
+                    print("The dragon is dead! You win!")
+                    break
+            else:
+                print("With no weapon you quickly die.")
+                print("--- Game Over ---")
+                break
+        
+        else:
+            print("You leave the beast alone and retreat the way you came.")
+            continue
